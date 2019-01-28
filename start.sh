6f9516e1bf34491d3bc66657c7d0b65fe1586685
Patrice Ferlet <patrice.ferlet@smile.fr>
2019-01-28 16:38:23 +0100

    Comments, and use tmp var file for pa module id

--- start.sh
+++ start.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 SEOPT=""
 PRIV=""
+
+# Fedora, CentOS, and so on... needs some option to allow
+# container to hit local volumes
 selinuxenabled 2>&1 >/dev/null && SEOPT=":Z" && PRIV="--privileged"
 
 PAMODULE='/var/tmp/snips-pa-module'
 
 function unload_pa_module(){
-    [ -f $PAMODULE ] && pactl unload-module $(cat $PAMODULE)
+    # if snips-pa-module file exists, stop the module and remove
+    # the tmp file
+    [ -f $PAMODULE ] && pactl unload-module $(cat $PAMODULE) && rm -f $PAMODULE
+
+    # be sure pulse is alerted
     sleep 1
 }
 
 
 function load_pa_module(){
+    # unload previous module, then reload
+
     unload_pa_module
     mod=$(pactl load-module module-native-protocol-unix socket=/tmp/pulse.sock)
+
+    # save the module id in tmp path
     echo "$mod" > $PAMODULE
 }
 
 function _start_snips_pulse() {
-    DOCKERIP=$(ip -4 -o a| grep docker0 | awk '{print $4}' | cut -d/ -f1)
-
+    # load pulseaudio socket
     load_pa_module
 
+    # start docker container
     docker run --rm -it \
         --name snips \
         -v $1:/usr/share/snips/assistant$SEOPT \
@@ -30,6 +41,7 @@ function _start_snips_pulse() {
         -e GROUPID=$(id -g) $PRIV \
         metal3d/snips
 
+    # stop unix socket
     unload_pa_module
 
 }
