6d6b970a966c959348342410fde08e1fdff3c9d0
Patrice Ferlet <metal3d@gmail.com>
2019-06-17 18:56:34 +0200

    Fixup skill-server
    Only launch skill server if skills are deployed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,12 @@
 
 eval $(fixuid -q)
 
+
+_skill=0
+
 # deploy given skills
 if [ -d /home/user/deploy ] ; then
+    _skill=1
     cp -r /home/user/deploy/* /var/lib/snips/skills/
 
     for d in $(find /var/lib/snips/skills -maxdepth 1 -type d); do
@@ -17,6 +21,7 @@ fi
 # start mosquitto at a certain port
 mosquitto -d -p $PORT 2>&1 1>/dev/null
 
+
 # start supervisord to launch snips services
 supervisord 2>&1 1>/dev/null 
 if [ $? != 0 ]; then
@@ -24,6 +29,11 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
+if [ $_skill -eq 1 ]; then
+    echo "Launching skill-server"
+    nohup snips-skill-server &
+    sleep 1
+fi
 
 if [ "$1" == "" ]; then
     tail -f /var/log/supervisor/snips*.log 
